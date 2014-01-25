d9f0ddd041695c87c08c6e97c7a2f38491dd04b4
flagg19 <wmacosx@yahoo.it>
2014-01-25 16:53:37 +0100

    add some const string

--- PingTester/Strings.cs
+++ PingTester/Strings.cs
@@ -8,10 +8,14 @@ namespace PingTester
 {
     public static class Strings
     {
+        public const string LifeCycleFormNotRunning = "Not running";
+        public const string LifeCycleFormRunning = "Running";
+        public const string LifeCycleFormStopping = "Stopping... please wait";
+        public const string LifeCycleExitError = "Stop execution before closing the application.";
+
         public const string ImportWarningMessage = "Current data will be overwritten by file data, continue?";
         public const string ChartError = "No data to be charted yet.";
         public const string GenericWarningTitle = "Warning!";
         public const string ExportError = "No data to be exported yet.";
-        public const string LifeCycleExitError = "Stop execution before closing the application.";
     }
 }
